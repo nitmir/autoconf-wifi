f3715b281fcdd347c253952801968668a4f5ee8e
Valentin Samir <samir@crans.org>
2013-07-17 17:08:53 +0200

    Licence disclamers

--- cert.ml
+++ cert.ml
@@ -1,3 +1,14 @@
+(*************************************************************************************)
+(* This program is distributed in the hope that it will be useful, but WITHOUT       *)
+(* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS     *)
+(* FOR A PARTICULAR PURPOSE.  See the GNU General Public License version 2 for       *)
+(* more details.                                                                     *)
+(*                                                                                   *)
+(* You should have received a copy of the GNU General Public License version 2       *)
+(* along with this program; if not, write to the Free Software Foundation, Inc., 51  *)
+(* Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.                          *)
+(*************************************************************************************)
+
 (* list of (store, comment, certificate) where store is the store where windows shall stores the certificate and comment a string display to the user after importinf the certificate. *)
 let certificates = [
 (* Ca Cert class3 certificate *)
